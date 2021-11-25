Mellanox/qemu

commit 28d9065692590cddb7a03119dae90aac16ddbe96

io: support peek function
This patch introduced peek function used to get bytes ready to read.

Signed-off-by: Xueming Li <xuemingl@mellanox.com>

chardev/char-socket.c
@@ -486,6 +486,14 @@ static gboolean tcp_chr_hup(QIOChannel *channel,
     return G_SOURCE_REMOVE;
 }
 
+static int tcp_chr_peek(Chardev *chr)
+{
+    SocketChardev *s = SOCKET_CHARDEV(chr);
+    QIOChannelClass *klass = QIO_CHANNEL_GET_CLASS(s->ioc);
+
+    return klass->io_peek(s->ioc, NULL);
+}
+
 static int tcp_chr_sync_read(Chardev *chr, const uint8_t *buf, int len)
 {
     SocketChardev *s = SOCKET_CHARDEV(chr);
@@ -1184,6 +1192,7 @@ static void char_socket_class_init(ObjectClass *oc, void *data)
     cc->open = qmp_chardev_open_socket;
     cc->chr_wait_connected = tcp_chr_wait_connected;
     cc->chr_write = tcp_chr_write;
+    cc->chr_peek = tcp_chr_peek;
     cc->chr_sync_read = tcp_chr_sync_read;
     cc->chr_disconnect = tcp_chr_disconnect;
     cc->get_msgfds = tcp_get_msgfds;

include/chardev/char.h
@@ -235,6 +235,7 @@ typedef struct ChardevClass {
                  bool *be_opened, Error **errp);
 
     int (*chr_write)(Chardev *s, const uint8_t *buf, int len);
+    int (*chr_peek)(Chardev *s);
     int (*chr_sync_read)(Chardev *s, const uint8_t *buf, int len);
     GSource *(*chr_add_watch)(Chardev *s, GIOCondition cond);
     void (*chr_update_read_handler)(Chardev *s);

include/io/channel.h
@@ -111,6 +111,7 @@ struct QIOChannelClass {
                          int *fds,
                          size_t nfds,
                          Error **errp);
+    int (*io_peek)(QIOChannel *ioc, Error **errp);
     ssize_t (*io_readv)(QIOChannel *ioc,
                         const struct iovec *iov,
                         size_t niov,

io/channel-socket.c
@@ -26,6 +26,8 @@
 #include "trace.h"
 #include "qapi/clone-visitor.h"
 
+#include <sys/ioctl.h>
+
 #define SOCKET_MAX_FDS 16
 
 SocketAddress *
@@ -453,6 +455,20 @@ static void qio_channel_socket_copy_fds(struct msghdr *msg,
     }
 }
 
+static int qio_channel_socket_peek(QIOChannel *ioc, Error **errp)
+{
+    QIOChannelSocket *sioc = QIO_CHANNEL_SOCKET(ioc);
+    int ret;
+    int bytes;
+
+    ret = ioctl(sioc->fd, FIONREAD, &bytes);
+    if (ret < 0) {
+        error_setg_errno(errp, errno, "Unable to peek from socket");
+        return -1;
+    }
+
+    return bytes;
+}
 
 static ssize_t qio_channel_socket_readv(QIOChannel *ioc,
                                         const struct iovec *iov,
@@ -755,6 +771,7 @@ static void qio_channel_socket_class_init(ObjectClass *klass,
     QIOChannelClass *ioc_klass = QIO_CHANNEL_CLASS(klass);
 
     ioc_klass->io_writev = qio_channel_socket_writev;
+    ioc_klass->io_peek = qio_channel_socket_peek;
     ioc_klass->io_readv = qio_channel_socket_readv;
     ioc_klass->io_set_blocking = qio_channel_socket_set_blocking;
     ioc_klass->io_close = qio_channel_socket_close;

commit ed47c5aa95dd7ea7c56ee2ff4dbf82703c0dda0d

io: fix io peek on disconnection
If vhost client killed, socket is removed, segment fault when access
socket.

Fixes: 28d906569259 ("io: support peek function"

Signed-off-by: Xueming Li <xuemingl@nvidia.com>

chardev/char-socket.c
@@ -489,8 +489,11 @@ static gboolean tcp_chr_hup(QIOChannel *channel,
 static int tcp_chr_peek(Chardev *chr)
 {
     SocketChardev *s = SOCKET_CHARDEV(chr);
-    QIOChannelClass *klass = QIO_CHANNEL_GET_CLASS(s->ioc);
+    QIOChannelClass *klass;
 
+    if (!s->ioc)
+        return -1;
+    klass = QIO_CHANNEL_GET_CLASS(s->ioc);
     return klass->io_peek(s->ioc, NULL);
 }
 