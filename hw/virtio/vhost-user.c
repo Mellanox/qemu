e7f9776c0c88ce7391cea0c3a270008925fb4c0b
Yajun Wu <yajunw@nvidia.com>
2022-04-19 10:33:09 +0300

    vhost-user: increase progress slave message timeout to 10s
    Signed-off-by: Yajun Wu <yajunw@nvidia.com>

--- hw/virtio/vhost-user.c
+++ hw/virtio/vhost-user.c
@@ -249,8 +249,8 @@ static int vhost_user_progress_slave(struct vhost_user *u) {
         /* no message, peek client message */
         if (vhost_for_each_device(vhost_dev_slave_read))
             continue; /* got slave message, no sleep */
-        g_usleep(100);
-        if (++timeout > 30000) {
+        g_usleep(1000);
+        if (++timeout > 10000) {
             error_report("Read vq %d timeout", u->dev->vq_index);
             return -1; /* timeout */
         }
