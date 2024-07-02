Mellanox/qemu

commit 3bfad9878a6035abf45c58c0f656028b19ae70a6

virtio-net: remove qemu datapath handler for vhost-user
When vhost device stop, default QEMU datapath handlers are installed
on kick event. QEMU and vhost user backend can dealing datapath at
the same time(suppose backend is still on when vhost sock closed).

	#2  0x000055a160a37123 in virtqueue_drop_all (vq=0x55a164081e58) at ../hw/virtio/virtio.c:1885
	#3  0x000055a1609f6bc2 in virtio_net_drop_tx_queue_data (vdev=0x55a164075720, vq=0x55a164081e58) at ../hw/net/virtio-net.c:377
	#4  0x000055a1609fcfb0 in virtio_net_handle_tx_bh (vdev=0x55a164075720, vq=0x55a164081e58)at ../hw/net/virtio-net.c:2873
	#5  0x000055a160a38285 in virtio_queue_notify_vq (vq=0x55a164081e58) at ../hw/virtio/virtio.c:2268
	#6  0x000055a160a3b7ce in virtio_queue_host_notifier_read (n=0x55a164081ecc) at ../hw/virtio/virtio.c:3671
	#7  0x000055a160cd188f in aio_dispatch_handler (ctx=0x55a162b21c80, node=0x55a164e02070) at ../util/aio-posix.c:372
	#8  0x000055a160cd1a2a in aio_dispatch_handlers (ctx=0x55a162b21c80) at ../util/aio-posix.c:414
	#9  0x000055a160cd1a80 in aio_dispatch (ctx=0x55a162b21c80) at ../util/aio-posix.c:424
	#10 0x000055a160ceeb19 in aio_ctx_dispatch (source=0x55a162b21c80, callback=0x0, user_data=0x0) at ../util/async.c:360
	#11 0x00007f6b0d57195d in g_main_context_dispatch () from target:/lib64/libglib-2.0.so.0
	#12 0x000055a160cf009a in glib_pollfds_poll () at ../util/main-loop.c:287
	#13 0x000055a160cf0114 in os_host_main_loop_wait (timeout=0) at ../util/main-loop.c:310
	#14 0x000055a160cf0219 in main_loop_wait (nonblocking=0) at ../util/main-loop.c:589
	#15 0x000055a16082dccc in qemu_main_loop () at ../system/runstate.c:783
	#16 0x000055a160ad6083 in qemu_default_main () at ../system/main.c:37
	#17 0x000055a160ad60be in main (argc=83, argv=0x7ffe4bcf77b8) at ../system/main.c:48

This fix can be a dirty work around: if backend is vhost-user, don't
install the kick event handlers.

Signed-off-by: Yajun Wu <yajunw@nvidia.com>

hw/net/virtio-net.c
@@ -2909,9 +2909,15 @@ static void virtio_net_tx_bh(void *opaque)
 static void virtio_net_add_queue(VirtIONet *n, int index)
 {
     VirtIODevice *vdev = VIRTIO_DEVICE(n);
+    NetClientState *nc = qemu_get_subqueue(n->nic, index);
 
-    n->vqs[index].rx_vq = virtio_add_queue(vdev, n->net_conf.rx_queue_size,
-                                           virtio_net_handle_rx);
+    if (nc->peer && nc->peer->info->type == NET_CLIENT_DRIVER_VHOST_USER) {
+        n->vqs[index].rx_vq = virtio_add_queue(vdev, n->net_conf.rx_queue_size,
+                                               NULL);
+    } else {
+        n->vqs[index].rx_vq = virtio_add_queue(vdev, n->net_conf.rx_queue_size,
+                                               virtio_net_handle_rx);
+    }
 
     if (n->net_conf.tx && !strcmp(n->net_conf.tx, "timer")) {
         n->vqs[index].tx_vq =
@@ -2921,11 +2927,17 @@ static void virtio_net_add_queue(VirtIONet *n, int index)
                                               virtio_net_tx_timer,
                                               &n->vqs[index]);
     } else {
-        n->vqs[index].tx_vq =
-            virtio_add_queue(vdev, n->net_conf.tx_queue_size,
-                             virtio_net_handle_tx_bh);
-        n->vqs[index].tx_bh = qemu_bh_new_guarded(virtio_net_tx_bh, &n->vqs[index],
-                                                  &DEVICE(vdev)->mem_reentrancy_guard);
+        if (nc->peer && nc->peer->info->type == NET_CLIENT_DRIVER_VHOST_USER) {
+            n->vqs[index].tx_vq =
+                virtio_add_queue(vdev, n->net_conf.tx_queue_size,
+                                 NULL);
+        } else {
+            n->vqs[index].tx_vq =
+                virtio_add_queue(vdev, n->net_conf.tx_queue_size,
+                                 virtio_net_handle_tx_bh);
+            n->vqs[index].tx_bh = qemu_bh_new_guarded(virtio_net_tx_bh, &n->vqs[index],
+                                                      &DEVICE(vdev)->mem_reentrancy_guard);
+        }
     }
 
     n->vqs[index].tx_waiting = 0;
@@ -2944,7 +2956,7 @@ static void virtio_net_del_queue(VirtIONet *n, int index)
     if (q->tx_timer) {
         timer_free(q->tx_timer);
         q->tx_timer = NULL;
-    } else {
+    } else if (q->tx_bh) {
         qemu_bh_delete(q->tx_bh);
         q->tx_bh = NULL;
     }
@@ -3771,6 +3783,17 @@ static void virtio_net_device_realize(DeviceState *dev, Error **errp)
     n->net_conf.tx_queue_size = MIN(virtio_net_max_tx_queue_size(n),
                                     n->net_conf.tx_queue_size);
 
+    if (n->netclient_type) {
+        /*
+         * Happen when virtio_net_set_netclient_name has been called.
+         */
+        n->nic = qemu_new_nic(&net_virtio_info, &n->nic_conf,
+                              n->netclient_type, n->netclient_name, n);
+    } else {
+        n->nic = qemu_new_nic(&net_virtio_info, &n->nic_conf,
+                              object_get_typename(OBJECT(dev)), dev->id, n);
+    }
+
     for (i = 0; i < n->max_queue_pairs; i++) {
         virtio_net_add_queue(n, i);
     }
@@ -3783,17 +3806,6 @@ static void virtio_net_device_realize(DeviceState *dev, Error **errp)
                               QEMU_CLOCK_VIRTUAL,
                               virtio_net_announce_timer, n);
     n->announce_timer.round = 0;
-
-    if (n->netclient_type) {
-        /*
-         * Happen when virtio_net_set_netclient_name has been called.
-         */
-        n->nic = qemu_new_nic(&net_virtio_info, &n->nic_conf,
-                              n->netclient_type, n->netclient_name, n);
-    } else {
-        n->nic = qemu_new_nic(&net_virtio_info, &n->nic_conf,
-                              object_get_typename(OBJECT(dev)), dev->id, n);
-    }
 
     for (i = 0; i < n->max_queue_pairs; i++) {
         n->nic->ncs[i].do_not_pad = true;

commit 40bfd06a1028209e643789cb532881f9e429c274

virtio-net: fix tx_bh check
Add more check for q->tx_bf before use.

Fixes: 3bfad9878a60 ("virtio-net: remove qemu datapath handler for vhost-user")
Signed-off-by: Yajun Wu <yajunw@nvidia.com>

hw/net/virtio-net.c
@@ -417,13 +417,13 @@ static void virtio_net_set_status(struct VirtIODevice *vdev, uint8_t status)
             if (q->tx_timer) {
                 timer_mod(q->tx_timer,
                                qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + n->tx_timeout);
-            } else {
+            } else if (q->tx_bh) {
                 qemu_bh_schedule(q->tx_bh);
             }
         } else {
             if (q->tx_timer) {
                 timer_del(q->tx_timer);
-            } else {
+            } else if (q->tx_bh) {
                 qemu_bh_cancel(q->tx_bh);
             }
             if ((n->status & VIRTIO_NET_S_LINK_UP) == 0 &&